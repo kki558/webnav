4bd2d8ca3dc570793b19390f0fdfef335434895d
kki558 <57279172+kki558@users.noreply.github.com>
2023-08-19 11:50:14 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var txt = document.getElementByClassName("btns");
+var input = document.getElementByClassName("search-btn");
+function zt () {	window.location.href="https://baidu.com/s?from=1022282z&word=" + input.value;
+}
